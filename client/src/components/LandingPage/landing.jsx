2768f56cf368f26b1fb93e2e264848323ae6976c
DiegoS <sdiegoss@gmail.com>
2023-08-11 14:10:52 +0100

    .

--- client/src/components/LandingPage/landing.jsx
+++ client/src/components/LandingPage/landing.jsx
@@ -13,9 +13,8 @@ export default function LandingPage() {
                 <div className={style.imgbox}>
                 <div className={style.centro}>
                     <img src={gamezone} alt='landing' className={style.img} />
-                <h1 className={style.title} > Welcome to my VideoGames Store </h1>
-                    <Link to='/Home' >
-                        <button className={style.button}> Come In ! </button>
+                            <Link to='/Home' >
+                        <button className={style.button}> Come In! </button>
                     </Link>
                 </div>
                 </div>

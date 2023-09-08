c5672eed4808d968510cb3e6c8324606b04235f5
Diego Sapia <sdiegoss@gmail.com>
2023-09-08 14:53:32 +0100

    Update landing.jsx

--- client/src/components/LandingPage/landing.jsx
+++ client/src/components/LandingPage/landing.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './landing.module.css';
-import gamezone from './gamezone.png';
 
 
 export default function LandingPage() {
     return (
-    
-    <div className={style.all}>
-        <div className={style.landing}>
-            <div className={style.imgC}>
-                <div className={style.imgbox}>
-                <div className={style.centro}>
-                    <img src={gamezone} alt='landing' className={style.img} />
-                <h1 className={style.title} > </h1>
-                    <Link to='/Home' >
-                        <button className={style.button}> Come In! </button>
-                    </Link>
-                </div>
-                </div>
-            </div>
-        </div>
-     </div>
+        <main className={style.all}>
+            <Link to='/Home' className={style.imgC}>
+                <div className={style.button}>  </div>
+            </Link>
+        </main>
     )
 };

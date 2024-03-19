6dfa2db587c0b547c394d31b913fd2a0c1ea3d0f
DiegoS <sdiegoss@gmail.com>
2024-03-19 19:43:42 +0100

    actualizacion del renderizado del nav en detail, estilos y estructura de details

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,28 @@
-import Landing from './components/LandingPage/landing';
-import { Routes,Route, useLocation, } from 'react-router-dom';
-import React, { useEffect } from 'react';
-import Home from '../src/components/Home/home';
-import Detail from './components/Details/detail';
- import Create from './components/Create/create';
- import Nav from './components/Nav/nav';
-import axios from 'axios';
-
+import Landing from "./components/LandingPage/landing";
+import { Routes, Route, useLocation } from "react-router-dom";
+import React from "react";
+import Home from "../src/components/Home/home";
+import Detail from "./components/Details/detail";
+import Create from "./components/Create/create";
+import Nav from "./components/Nav/nav";
 
 function App() {
-  useEffect(() => {
-    // Cambia el título de la página web
-    document.title = "VideoGame DS";
-
-    // También puedes utilizar variables de estado de React
-    // para cambiar el título dinámicamente
-    // Ejemplo: document.title = `Mi Aplicación - ${miVariableDeEstado}`;
-  }, []); 
- 
- const {pathname} = useLocation()
-
-
+  document.title = "VideoGame DS";
+  const { pathname } = useLocation();
 
   return (
     <div>
       <div>
-        {pathname !== '/' && <Nav />}
+        {pathname !== "/" && !pathname.startsWith("/detail/") && <Nav />}
       </div>
       <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/detail/:id' element={<Detail/>} />
-         <Route path='create' element={<Create/>} /> 
-       
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="create" element={<Create />} />
       </Routes>
     </div>
   );
 }
-
 
 export default App;
